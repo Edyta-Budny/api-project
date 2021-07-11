13529e59330705aebd8848df7541323617bd02ca
Edyta Kaleta <edyta.anna.kaleta@gmail.com>
2021-07-12 01:00:37 +0200

    fix git push heroku error #1

--- API/settings.py
+++ API/settings.py
@@ -67,7 +67,12 @@ WSGI_APPLICATION = 'API.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = []
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
